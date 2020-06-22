df5698898e422aa3762a66c7557888d722fb004b
Muhammad Saqlain Saleem <Miansaqlain52@gmail.com>
2020-06-22 10:12:24 -0700

    Update AddTransaction.js

--- expence-tracker-app/src/components/AddTransaction.js
+++ expence-tracker-app/src/components/AddTransaction.js
@@ -22,7 +22,7 @@ export const AddTransaction = () => {
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e) => setText(e.target.value)}
-          placeholder="Enter text..." />
+          placeholder="Enter text here..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount"
